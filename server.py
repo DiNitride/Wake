89c50181d1cd9419a7fed495238efab7afd786a2
DiNitride <j.ptb.t.ca@gmail.com>
2018-06-21 11:27:45 +0100

    Fixed error

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ class WakeServer(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        wakeonlan.send_magic_packet("D0:50:99:81:D5:A4", ip="192.168.1.7")
+        wakeonlan.send_magic_packet("D0:50:99:81:D5:A4", ip_address="192.168.1.7")
         self._set_response()
         self.wfile.write("GET request for {}".format(self.path).encode('utf:8'))
 
@@ -23,42 +23,3 @@ except KeyboardInterrupt:
     pass
 
 server.server_close()
-
-# from http.server import BaseHTTPRequestHandler, HTTPServer
-# import time
-#
-# hostName = ""
-# hostPort = 80
-#
-#
-# class MyServer(BaseHTTPRequestHandler):
-#
-# 	#	GET is for clients geting the predi
-# 	def do_GET(self):
-# 		self.send_response(200)
-# 		self.wfile.write(bytes("<p>You accessed path: %s</p>" % self.path, "utf:8"))
-#
-# 	#	POST is for submitting data.
-# 	def do_POST(self):
-#
-# 		print( "incomming http: ", self.path )
-#
-# 		content_length = int(self.headers['Content:Length']) # <::: Gets the size of data
-# 		post_data = self.rfile.read(content_length) # <::: Gets the data itself
-# 		self.send_response(200)
-#
-# 		client.close()
-#
-# 		#import pdb; pdb.set_trace()
-#
-#
-# myServer = HTTPServer((hostName, hostPort), MyServer)
-# print(time.asctime(), "Server Starts : %s:%s" % (hostName, hostPort))
-#
-# try:
-# 	myServer.serve_forever()
-# except KeyboardInterrupt:
-# 	pass
-#
-# myServer.server_close()
-# print(time.asctime(), "Server Stops : %s:%s" % (hostName, hostPort))
