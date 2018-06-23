f6869b7621e1c2335ae2ae36b6c9b684e93983ba
DiNitride <j.ptb.t.ca@gmail.com>
2018-06-23 09:01:55 +0100

    Added endpoint requirement

--- wake_server.py
+++ wake_server.py
@@ -10,9 +10,10 @@ class WakeServer(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        wakeonlan.send_magic_packet("D0:50:99:81:D5:A4", ip_address="192.168.1.7")
-        self._set_response()
-        self.wfile.write("GET request for {}".format(self.path).encode('utf:8'))
+        if self.path == "/wake":
+            wakeonlan.send_magic_packet("D0:50:99:81:D5:A4", ip_address="192.168.1.7")
+            self._set_response()
+            self.wfile.write("GET request for {}".format(self.path).encode('utf:8'))
 
 
 server = HTTPServer(("0.0.0.0", 6578), WakeServer)
